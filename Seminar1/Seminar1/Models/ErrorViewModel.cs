402397f3adae456ce7519b25dab71233cd134d75
Stasya-a <68816694+Stasya-a@users.noreply.github.com>
2021-02-22 13:16:24 +0300

    Sem1

---
+++ Seminar1/Seminar1/Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+// бизнес логика
+using System;
+
+namespace Seminar1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
